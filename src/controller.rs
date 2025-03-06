9b0632435431516cd3a8d9630b76c37a316c756b
fwcd <fwcd@fwcd.dev>
2025-03-06 18:57:36 +0100

    Add experimental gamepad support

--- src/controller.rs
+++ src/controller.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use futures::{prelude::*, Stream};
-use lighthouse_client::protocol::{InputEvent, KeyEvent, ServerMessage};
+use lighthouse_client::protocol::{Direction, InputEvent, KeyEvent, ServerMessage};
 use tokio::sync::mpsc;
 
 use crate::message::{ControllerMessage, Key};
@@ -9,12 +9,39 @@ pub async fn run(
     mut stream: impl Stream<Item = lighthouse_client::Result<ServerMessage<InputEvent>>> + Unpin,
     tx: mpsc::Sender<ControllerMessage>,
 ) -> Result<()> {
+    let mut last_gamepad_key: Option<Key> = None;
+
     while let Some(msg) = stream.next().await {
         let input_event = msg?.payload;
+
         match input_event {
             InputEvent::Key(KeyEvent { code, down, .. }) => {
                 if let Some(key) = convert_key(&code) {
                     tx.send(ControllerMessage::Key { key, down }).await?;
+                }
+            },
+            InputEvent::Gamepad(gamepad) => {
+                let opt_key = if let Some(dir) = gamepad.left_direction() {
+                    Some(match dir {
+                        Direction::Up => Key::Letter('W'),
+                        Direction::Left => Key::Letter('A'),
+                        Direction::Down => Key::Letter('S'),
+                        Direction::Right => Key::Letter('D'),
+                    })
+                } else if let Some(dir) = gamepad.right_direction() {
+                    match dir {
+                        Direction::Left => Some(Key::ArrowLeft),
+                        Direction::Right => Some(Key::ArrowRight),
+                        _ => None,
+                    }
+                } else {
+                    None
+                };
+                if let Some(key) = opt_key {
+                    tx.send(ControllerMessage::Key { key, down: true }).await?;
+                    last_gamepad_key = Some(key);
+                } else if let Some(key) = last_gamepad_key.take() {
+                    tx.send(ControllerMessage::Key { key, down: false }).await?;
                 }
             },
             // TODO: Add gamepad input
